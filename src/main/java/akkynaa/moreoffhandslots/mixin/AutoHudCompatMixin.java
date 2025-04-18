6bb1e1e1afddcd63d9c41e189135d5d944413587
AkkynaA <akkynaa@outlook.com>
2025-04-18 19:08:13 +0200

    Chore: Disable auto hud compatibility for repo

--- src/main/java/akkynaa/moreoffhandslots/mixin/AutoHudCompatMixin.java
+++ src/main/java/akkynaa/moreoffhandslots/mixin/AutoHudCompatMixin.java
@@ -1,29 +1,29 @@
-package akkynaa.moreoffhandslots.mixin;
-
-import akkynaa.moreoffhandslots.compat.AutoHudCompat;
-import mod.crend.autohud.forge.AutoHudModEvents;
-import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-
-@Mixin(AutoHudModEvents.class)
-public class AutoHudCompatMixin {
-
-
-    @Inject(
-            method = "onClientSetup",
-            at = @At("TAIL"),
-            remap = false,
-            require = 0
-    )
-    private static void onClientSetup(FMLClientSetupEvent event, CallbackInfo ci) {
-        if (ModList.get().isLoaded("autohud")) {
-            AutoHudCompat.register();
-        }
-    }
-
-}
+//package akkynaa.moreoffhandslots.mixin;
+//
+//import akkynaa.moreoffhandslots.compat.AutoHudCompat;
+//import mod.crend.autohud.forge.AutoHudModEvents;
+//import net.minecraftforge.fml.ModList;
+//import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.injection.At;
+//import org.spongepowered.asm.mixin.injection.Inject;
+//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+//
+//
+//@Mixin(AutoHudModEvents.class)
+//public class AutoHudCompatMixin {
+//
+//
+//    @Inject(
+//            method = "onClientSetup",
+//            at = @At("TAIL"),
+//            remap = false,
+//            require = 0
+//    )
+//    private static void onClientSetup(FMLClientSetupEvent event, CallbackInfo ci) {
+//        if (ModList.get().isLoaded("autohud")) {
+//            AutoHudCompat.register();
+//        }
+//    }
+//
+//}
