15d1f45f56ef0fb58a745aedc2ca508834366b0f
AkkynaA <akkynaa@outlook.com>
2026-03-23 21:37:35 +0100

    Clean repo for main

--- src/main/java/akkynaa/moreoffhandslots/capability/ModCapabilities.java
+++
@@ -1,9 +0,0 @@
-package akkynaa.moreoffhandslots.capability;
-
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.CapabilityManager;
-import net.minecraftforge.common.capabilities.CapabilityToken;
-
-public class ModCapabilities {
-    public static final Capability<OffhandPosition> OFFHAND_POSITION = CapabilityManager.get(new CapabilityToken<>() {});
-}
